92f6d55f81e79c12484a8be4e1ebabbd5fb15623
Alex Esaki <apachecordovax@gmail.com>
2024-02-08 12:57:11 +0100

    chat messages component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 import { db } from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -40,24 +41,35 @@ const ChannelIdPage = async ({
     }
 
     return (
-        <div className="bg-white dark:bg-[#313338] h-full">
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-screen">
             <ChatHeader
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
             />
-            <div className="flex-1">Future Messages</div>
-            <div className="absolute bottom-0 w-[1050px]">
-                <ChatInput
-                    name={channel.name}
-                    type="channel"
-                    apiUrl="/api/socket/messages"
-                    query={{
-                        channelId: channel.id,
-                        serverId: channel.serverId,
-                    }}
-                />
-            </div>
+            <ChatMessages
+                member={member}
+                name={channel.name}
+                chatId={channel.id}
+                type="channel"
+                apiUrl="/api/messages"
+                socketUrl="/api/socket/messages"
+                socketQuery={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
+                paramKey="channelId"
+                paramValue={channel.id}
+            />
+            <ChatInput
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
+            />
         </div>
     );
 }
